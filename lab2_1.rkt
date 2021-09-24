582016c1e1c7948c9acedadf1191f9e8946629a4
Leriatod <artem.viznuk@gmail.com>
2021-09-24 12:34:53 +0300

    Modified files for lab2

--- lab2_1.rkt
+++ lab2_1.rkt
@@ -1,24 +1,29 @@
 #lang racket
 
+; calculating sin value by using taylor series
 (define (sine x)
   (define error 0.000001)
   (define x-square (* x x))
-  
+
+  (define prev-taylor-el 0)
   (define cur-taylor-el x)
   (define curSum x)
   (define n 1)
   
-  
+  ; tail-recursive function for sin calculation
   (define (sin-tail)
+    (set! prev-taylor-el cur-taylor-el)
     (set!
       cur-taylor-el
       (/ (* cur-taylor-el -1 x-square ) (+ n 1) (+ n 2))
     )
     (set! curSum (+ curSum cur-taylor-el))
     (set! n (+ n 2))
-    
+
+    ; if |cur taylor element - prev taylor element| < error
+    ; return cur sum of taylor series
     (cond
-      [(< (abs (- curSum (sin x))) error) curSum]
+      [(< (abs (- cur-taylor-el prev-taylor-el)) error) curSum]
       [else (sin-tail)]
     )
   )
@@ -26,14 +31,26 @@
   (sin-tail)
 )
 
-(define (y x)
+; returns sine(x)-sine(x/2) for 0<x<1
+; and sine(x)^3 - sine(x + 0.125) for -2<=x<=0
+(define (y1 x)
   (cond
     [(and (< 0 x) (< x 1)) (- (sine x) (sine (/ x 2)) )]
     [(and (<= -2 x) (<= x 0)) (- (expt (sine x) 3) (sine (+ x 0.125)))]
   )
 )
 
-(define (y-range x-start x-end step)
+; the same as y1, but with libary sin
+(define (y2 x)
+  (cond
+    [(and (< 0 x) (< x 1)) (- (sin x) (sin (/ x 2)) )]
+    [(and (<= -2 x) (<= x 0)) (- (expt (sin x) 3) (sin (+ x 0.125)))]
+  )
+)
+
+; returns list of y values from x-start to x-end (included)
+; with defined step
+(define (y-range x-start x-end step y)
   (define (y-range-tail x-start x-end step [y-list '()])
     (cond
       [(> x-start x-end) y-list]
@@ -48,5 +65,8 @@
   (y-range-tail x-start x-end step)
 )
 
+; printing y values for custom sin function
+(y-range -2 2 0.5 y1)
 
-(y-range -2 2 0.5)
+; printing y values for library sin function
+(y-range -2 2 0.5 y2) 
--- lab2_2.rkt
+++ lab2_2.rkt
@@ -1,14 +1,43 @@
 #lang racket
 
-(define (calculate-fraction [depth 100])
+(define (square x)
+  (* x x)
+)
+
+; функція для обрахування нескінченного дробу за правилом:
+; 4 / (1 + 1 / (3 + 2^2 / (5 + 3^2 / (7 + ...)))
+(define (calculate-fraction)
+  (define error 0.01)
+  
+  ; визначаємо рекурсію
+  ; number - перший доданок в знаменнику
+  ; nominator - значення в чисельнику
   (define (calculate-fraction-recursive [number 1] [nominator 1])
+
+    ; умова завершення рекурсивних викликів
     (cond
-      [(>= nominator depth) (+ number (* nominator nominator))]
-      [else (+ number (/ (* nominator nominator) (calculate-fraction-recursive (+ number 2) (+ nominator 1)) ))]
+      [(< (abs (-
+           (/
+            (square nominator)
+            (+ number 2 (square (+ nominator 1)))
+           )
+           1
+          ))
+          error
+       )
+       (+ number (square nominator))]
+      [else
+       (+ number
+          (/
+           (square nominator)
+           (calculate-fraction-recursive (+ number 2) (+ nominator 1))
+          )
+       )]
     )
   )
 
+  ; виклик рекурсивної функції для повернення результату
   (/ 4. (calculate-fraction-recursive))
 )
 
-(calculate-fraction 100)
+(calculate-fraction)
